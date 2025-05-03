00b39f62df2019ba48d88ed9e1f89fc4e3ac318d
raoofkp <raoofa109@gmail.com>
2025-05-03 23:09:42 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { } from 'react'
+import './App.css'
+import ToDo from './componet/ToDoApp/ToDo.jsx'
+
+
+function App() {
+  return (
+    <>
+  <ToDo />     
+    </>
+  )
+}
+
+export default App
---
+++ src/componet/ToDoApp/AddTask.jsx
@@ -0,0 +1,30 @@
+import React, { useState } from 'react'
+
+const AddTask = ({addTask}) => {
+    const [value,setValue]=useState("")
+
+    const addItem=()=>{
+        addTask(value)
+        setValue("")
+    }
+
+  return (
+    <>
+     <div className='input-container'>
+        <input 
+        className='input'
+        placeholder='Add Task'
+        type="text" 
+        value={value}
+        onChange={(e)=>{
+            setValue(e.target.value)
+        }}
+        required
+        />
+        <button onClick={addItem} className='add-btn'>ADD</button>
+     </div>
+    </>
+  )
+}
+
+export default AddTask
